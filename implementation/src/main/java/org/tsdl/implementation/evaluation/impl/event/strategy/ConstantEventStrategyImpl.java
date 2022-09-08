983baa0160a5624ddc403e4bedae16a0a811a45c
Raffael Foidl <raffael.foidl@gmail.com>
2022-09-08 08:57:44 +0200

    Remove duplication across constant and increase event

--- implementation/src/main/java/org/tsdl/implementation/evaluation/impl/event/strategy/ConstantEventStrategyImpl.java
+++ implementation/src/main/java/org/tsdl/implementation/evaluation/impl/event/strategy/ConstantEventStrategyImpl.java
@@ -1,41 +1,26 @@
 package org.tsdl.implementation.evaluation.impl.event.strategy;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.stream.Stream;
-import org.tsdl.implementation.evaluation.impl.choice.AnnotatedTsdlPeriodImpl;
 import org.tsdl.implementation.evaluation.impl.common.TsdlIdentifierImpl;
 import org.tsdl.implementation.evaluation.impl.event.TsdlEventImpl;
 import org.tsdl.implementation.evaluation.impl.filter.argument.TsdlLiteralScalarArgumentImpl;
 import org.tsdl.implementation.evaluation.impl.filter.deviation.AbsoluteAroundFilterImpl;
-import org.tsdl.implementation.factory.TsdlComponentFactory;
-import org.tsdl.implementation.math.Calculus;
-import org.tsdl.implementation.math.ContinuousRegression;
-import org.tsdl.implementation.math.impl.ContinuousRegressionImpl;
 import org.tsdl.implementation.math.impl.SummaryStatisticsImpl;
 import org.tsdl.implementation.model.choice.AnnotatedTsdlPeriod;
-import org.tsdl.implementation.model.common.TsdlIdentifier;
 import org.tsdl.implementation.model.event.TsdlEvent;
 import org.tsdl.implementation.model.event.TsdlEventStrategyType;
 import org.tsdl.implementation.model.event.definition.AndEventConnectiveImpl;
 import org.tsdl.implementation.model.event.definition.ConstantEvent;
 import org.tsdl.implementation.model.event.strategy.ConstantEventStrategy;
-import org.tsdl.implementation.model.event.strategy.SinglePointEventStrategy;
-import org.tsdl.infrastructure.common.Condition;
-import org.tsdl.infrastructure.common.Conditions;
 import org.tsdl.infrastructure.common.TsdlTimeUnit;
 import org.tsdl.infrastructure.model.DataPoint;
-import org.tsdl.infrastructure.model.TsdlPeriod;
 
 /**
  * Default implementation of {@link ConstantEventStrategy}.
  */
-public class ConstantEventStrategyImpl implements ConstantEventStrategy {
-  private static final Calculus CALCULUS = TsdlComponentFactory.INSTANCE.calculus();
-  private static final ContinuousRegression CONTINUOUS_REGRESSION = new ContinuousRegressionImpl();
+public class ConstantEventStrategyImpl extends ComplexEventStrategy implements ConstantEventStrategy {
   private static final double DERIVATIVE_THRESHOLD = 0.002; // maximal instantaneous rate of change: 0.2 %
-  private static final SinglePointEventStrategy EVENT_DETECTION_HELPER = new SinglePointEventStrategyImpl();
 
   @Override
   public List<AnnotatedTsdlPeriod> detectPeriods(List<DataPoint> dataPoints, List<TsdlEvent> events) {
@@ -58,25 +43,10 @@ public class ConstantEventStrategyImpl implements ConstantEventStrategy {
     );
 
     // heuristic
-    var periodCandidates = replaceEvent(
-        EVENT_DETECTION_HELPER
-            .detectPeriods(derivative, List.of(derivativeEvent))
-            .stream()
-            .filter(p -> !p.period().isEmpty() && !p.period().start().equals(p.period().end())),
-        constantEvent.identifier()
-    );
-
-    // get data points in period candidates (determine ps(p_i))
-    var dpsPerPeriod = new HashMap<TsdlPeriod, List<DataPoint>>();
-    for (var dp : dataPoints) {
-      var currentPeriod = periodCandidates.stream().filter(p -> p.period().contains(dp.timestamp())).findFirst();
-      currentPeriod.ifPresent(p -> dpsPerPeriod.computeIfAbsent(p.period(), (k) -> new ArrayList<>()).add(dp));
-    }
-
-    Conditions.checkEquals(Condition.ARGUMENT, dpsPerPeriod.size(), periodCandidates.size(), "Could find data points from every data period.");
+    var periodCandidates = findPeriodCandidates(derivative, derivativeEvent, constantEvent.identifier());
+    var dpsPerPeriod = groupDataPointsByPeriod(dataPoints, periodCandidates);
 
     // now semantics definition to filter out invalid periods
-
     // regc
     var satRegc = new ArrayList<AnnotatedTsdlPeriod>();
     for (var candidate : periodCandidates) {
@@ -105,14 +75,5 @@ public class ConstantEventStrategyImpl implements ConstantEventStrategy {
     }
 
     return satDevc;
-  }
-
-  private List<AnnotatedTsdlPeriod> replaceEvent(Stream<AnnotatedTsdlPeriod> periods, TsdlIdentifier newEventIdentifier) {
-    return periods
-        .map(p -> (AnnotatedTsdlPeriod) new AnnotatedTsdlPeriodImpl(p.period(),
-            newEventIdentifier,
-            p.priorDataPoint().orElse(null),
-            p.subsequentDataPoint().orElse(null)))
-        .toList();
   }
 }
--- implementation/src/main/java/org/tsdl/implementation/evaluation/impl/event/strategy/IncreaseEventStrategyImpl.java
+++ implementation/src/main/java/org/tsdl/implementation/evaluation/impl/event/strategy/IncreaseEventStrategyImpl.java
@@ -1,38 +1,25 @@
 package org.tsdl.implementation.evaluation.impl.event.strategy;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.stream.Stream;
-import org.tsdl.implementation.evaluation.impl.choice.AnnotatedTsdlPeriodImpl;
 import org.tsdl.implementation.evaluation.impl.common.TsdlIdentifierImpl;
 import org.tsdl.implementation.evaluation.impl.event.TsdlEventImpl;
 import org.tsdl.implementation.evaluation.impl.filter.NegatedSinglePointFilterImpl;
 import org.tsdl.implementation.evaluation.impl.filter.argument.TsdlLiteralScalarArgumentImpl;
 import org.tsdl.implementation.evaluation.impl.filter.threshold.LessThanFilterImpl;
-import org.tsdl.implementation.factory.TsdlComponentFactory;
-import org.tsdl.implementation.math.Calculus;
 import org.tsdl.implementation.model.choice.AnnotatedTsdlPeriod;
-import org.tsdl.implementation.model.common.TsdlIdentifier;
 import org.tsdl.implementation.model.event.TsdlEvent;
 import org.tsdl.implementation.model.event.TsdlEventStrategyType;
 import org.tsdl.implementation.model.event.definition.AndEventConnectiveImpl;
 import org.tsdl.implementation.model.event.definition.IncreaseEvent;
 import org.tsdl.implementation.model.event.strategy.IncreaseEventStrategy;
-import org.tsdl.implementation.model.event.strategy.SinglePointEventStrategy;
-import org.tsdl.infrastructure.common.Condition;
-import org.tsdl.infrastructure.common.Conditions;
 import org.tsdl.infrastructure.common.TsdlTimeUnit;
 import org.tsdl.infrastructure.model.DataPoint;
-import org.tsdl.infrastructure.model.TsdlPeriod;
 
 /**
  * Default implementation of {@link IncreaseEventStrategy}.
  */
-public class IncreaseEventStrategyImpl implements IncreaseEventStrategy {
-  private static final Calculus CALCULUS = TsdlComponentFactory.INSTANCE.calculus();
-  private static final SinglePointEventStrategy EVENT_DETECTION_HELPER = new SinglePointEventStrategyImpl();
-
+public class IncreaseEventStrategyImpl extends ComplexEventStrategy implements IncreaseEventStrategy {
   @Override
   public List<AnnotatedTsdlPeriod> detectPeriods(List<DataPoint> dataPoints, List<TsdlEvent> events) {
     var increaseEvent = events.get(0);
@@ -55,22 +42,8 @@ public class IncreaseEventStrategyImpl implements IncreaseEventStrategy {
         TsdlEventStrategyType.SINGLE_POINT_EVENT
     );
 
-    var periodCandidates = replaceEvent(
-        EVENT_DETECTION_HELPER
-            .detectPeriods(derivative, List.of(derivateEvent))
-            .stream()
-            .filter(p -> !p.period().isEmpty() && !p.period().start().equals(p.period().end())),
-        increaseEvent.identifier()
-    );
-
-    // get data points in period candidates (determine ps(p_i))
-    var dpsPerPeriod = new HashMap<TsdlPeriod, List<DataPoint>>();
-    for (var dp : dataPoints) {
-      var currentPeriod = periodCandidates.stream().filter(p -> p.period().contains(dp.timestamp())).findFirst();
-      currentPeriod.ifPresent(p -> dpsPerPeriod.computeIfAbsent(p.period(), (k) -> new ArrayList<>()).add(dp));
-    }
-
-    Conditions.checkEquals(Condition.ARGUMENT, dpsPerPeriod.size(), periodCandidates.size(), "Could find data points from every data period.");
+    var periodCandidates = findPeriodCandidates(derivative, derivateEvent, increaseEvent.identifier());
+    var dpsPerPeriod = groupDataPointsByPeriod(dataPoints, periodCandidates);
 
     // difc
     var difc = new ArrayList<AnnotatedTsdlPeriod>();
@@ -87,14 +60,5 @@ public class IncreaseEventStrategyImpl implements IncreaseEventStrategy {
     }
 
     return difc;
-  }
-
-  private List<AnnotatedTsdlPeriod> replaceEvent(Stream<AnnotatedTsdlPeriod> periods, TsdlIdentifier newEventIdentifier) {
-    return periods
-        .map(p -> (AnnotatedTsdlPeriod) new AnnotatedTsdlPeriodImpl(p.period(),
-            newEventIdentifier,
-            p.priorDataPoint().orElse(null),
-            p.subsequentDataPoint().orElse(null)))
-        .toList();
   }
 }
